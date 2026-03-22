e22af3ff1115ebdcc9f72ec15ba662c2030cbaa5
Beno-Jeffry-B <benojeffry08@gmail.com>
2026-03-22 18:02:54 +0530

    fix : country flag ui

--- src/utils/getFlagEmoji.ts
+++ src/utils/getFlagEmoji.ts
@@ -1,15 +1,15 @@
 const countryCodeMap: Record<string, string> = {
-  India: "🇮🇳",
-  Spain: "🇪🇸",
-  "United States": "🇺🇸",
-  Germany: "🇩🇪",
-  France: "🇫🇷",
-  Canada: "🇨🇦",
-  Australia: "🇦🇺",
-  China: "🇨🇳",
-  Japan: "🇯🇵",
-  Brazil: "🇧🇷",
-  "United Kingdom": "🇬🇧",
+  India: "IN",
+  Spain: "ES",
+  "United States": "US",
+  Germany: "DE",
+  France: "FR",
+  Canada: "CA",
+  Australia: "AU",
+  China: "CN",
+  Japan: "JP",
+  Brazil: "BR",
+  "United Kingdom": "GB",
 };
 
 export const getFlagEmoji = (country: string): string => {
